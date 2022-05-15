3af0336ce5b681d3d1db50e4d65ca5ce37ffa11b
Arni Magnusson <thisisarni@gmail.com>
2022-05-16 03:29:23 +1100

    Combine and tabulate

--- analysis.R
+++ analysis.R
@@ -1,24 +1,21 @@
 library(lattice)
 
-## 1  Calculate relative difference
+## 1  Read data
 
 x <- read.csv("results.csv")
 
-x <- x[order(x$Machine, x$OS, x$Test, x$ADMB),]
-x$GCC <- NULL
-
-v12 <- x$Runtime[x$ADMB == "12.3"]
-v13 <- x$Runtime[x$ADMB == "13.0-beta"]
-
-rel <- v13 / v12
-
-print(median(rel))
-
-## 2  Combine groups and plot
+## 2  Combine platforms
 
 x$Platform <- "Linux"
 x$Platform[grep("Windows", x$OS)] <- "Windows"
-
 y <- aggregate(Runtime ~ Platform + Test + ADMB, data=x, median)
+
+## 3  Calculate relative difference
+
+z <- xtabs(Runtime~paste(Test, "-", Platform) + ADMB, data=y)
+z <- as.data.frame(unclass(z))
+z$rel <- z$"13.0-beta" / z$"12.3"
+
+## 4  Plot
 
 barchart(Runtime ~ Test | Platform, groups=ADMB, data=y, col=3:2)
