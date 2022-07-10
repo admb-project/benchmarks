5d99586b21a5dbbaface22ce81a75a761186578a
Arni Magnusson <thisisarni@gmail.com>
2022-07-11 01:51:13 +1100

    Add July results and analysis

---
+++ 2022_07_10/analysis.R
@@ -0,0 +1,26 @@
+library(lattice)
+
+## Read data, set up graphical device
+x <- read.csv("results.csv")
+pdf("plot.pdf", width=16, height=10)
+
+## Detailed comparison
+col <- c(3, 2, 7)
+x <- x[order(x$Machine, x$OS, x$Test, x$ADMB),]
+x <- x[c("Machine", "OS", "Test", "ADMB", "Runtime")]
+xplot <- barchart(Runtime ~ Test | Machine + OS, groups=ADMB, data=x, col=col,
+                  main="Detailed comparison", ylab="Runtime (sec)",
+                  sub="Green = ADMB 12.3\nRed = ADMB 13.0-beta (July)")
+
+## Grouped comparison
+x$Platform <- "Linux"
+x$Platform[grep("Windows", x$OS)] <- "Windows"
+y <- aggregate(Runtime ~ ADMB + Platform + Test, data=x, median)
+yplot <- barchart(Runtime ~ Test | Platform, groups=ADMB, data=y, col=col,
+                  main="Grouped comparison", ylab="Runtime (sec)",
+                  sub="Green = ADMB 12.3\nRed = ADMB 13.0-beta (July)")
+
+## Export plots
+print(xplot, position=c(0.00, 0.05, 0.35, 0.95), more=TRUE)
+print(yplot, position=c(0.35, 0.05, 1.00, 0.95))
+dev.off()
