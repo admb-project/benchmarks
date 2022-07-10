70edabf9c20d7db93c2d08d85c536e97958bb6c7
Arni Magnusson <thisisarni@gmail.com>
2022-07-11 02:41:29 +1100

    Prepare subtitle before plotting

--- 2022_05_16/analysis.R
+++ 2022_05_16/analysis.R
@@ -6,19 +6,18 @@ pdf("plot.pdf", width=16, height=10)
 
 ## Detailed comparison
 col <- 3:2
+sub <- "Green = ADMB 12.3\nRed = ADMB 13.0-beta (May)"
 x <- x[order(x$Machine, x$OS, x$Test, x$ADMB),]
 x <- x[c("Machine", "OS", "Test", "ADMB", "Runtime")]
 xplot <- barchart(Runtime ~ Test | Machine + OS, groups=ADMB, data=x, col=col,
-                  main="Detailed comparison", ylab="Runtime (sec)",
-                  sub="Green = ADMB 12.3\nRed = ADMB 13.0-beta (May)")
+                  main="Detailed comparison", ylab="Runtime (sec)", sub=sub)
 
 ## Grouped comparison
 x$Platform <- "Linux"
 x$Platform[grep("Windows", x$OS)] <- "Windows"
 y <- aggregate(Runtime ~ ADMB + Platform + Test, data=x, median)
 yplot <- barchart(Runtime ~ Test | Platform, groups=ADMB, data=y, col=col,
-                  main="Grouped comparison", ylab="Runtime (sec)",
-                  sub="Green = ADMB 12.3\nRed = ADMB 13.0-beta (May)")
+                  main="Grouped comparison", ylab="Runtime (sec)", sub=sub)
 
 ## Export plots
 print(xplot, position=c(0.00, 0.05, 0.35, 0.95), more=TRUE)
